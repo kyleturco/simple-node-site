bdd383cca3e7d91559a93f5eb919f3dcce85b5fd
Kyle Turco <kyleturco@gmail.com>
2015-08-04 15:42:09 -0500

    start main route, main.ejs template, add img ffolder

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Requires
 var fs = require('fs');
 var express = require('express');
 var lessCSS = require('less-middleware');
@@ -9,3 +10,64 @@ var about = require('./routes/about');
 var contact = require('./routes/contact');
 
 var app = express();
+
+// Main section
+
+app.set('view engine', 'ejs');
+app.set('case sensitive routing', true);
+
+app.locals.title = 'The Wonders';
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(lessCSS('public'));
+
+var logStream = fs.createWriteStream('access.log', {flags: 'a'});
+app.use(morgan('combined', {stream: logStream}));
+app.use(morgan('dev'));
+
+app.use(function (req, res, next) {
+  var client = require('./lib/loggly')('incoming');
+
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: res.statusCode,
+    method: req.method
+  });
+  next();
+});
+
+app.use(express.static('styles'));
+
+app.use('/main', routes);
+app.use('/about', about);
+app.use('/contact', contact);
+
+app.use(function (req, res) {
+  res.status(403).send('Unauthorized!');
+});
+
+app.use(function (err, req, res, next) {
+  var client = require('./lib/loggly')('error');
+
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: res.statusCode,
+    method: req.method,
+    stackTrace: err.stack
+  });
+
+  // pass 4 arguments to create an error handling middleware
+  console.log('ERRRRRRRRRR', err.stack);
+  res.status(500).send('My Bad');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%d', host, port);
+});
